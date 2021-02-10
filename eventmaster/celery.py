4901a879bdb8c22dd75683cad5b098c115d6ad53
Kate <bbkisik@gmail.com>
2021-02-10 13:05:58 +0300

    fix

--- eventmaster/celery.py
+++ eventmaster/celery.py
@@ -2,8 +2,8 @@ import os
 
 from celery import Celery
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eventmaster.settings')
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eventmaster.Postgre_settings.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eventmaster.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eventmaster.Postgre_settings.settings')
 
 app = Celery('eventmaster')
 
