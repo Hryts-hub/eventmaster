00c02b4f1d11d447d48ee8294e3c8c0884e5862b
Kate <bbkisik@gmail.com>
2021-02-10 12:51:19 +0300

    fix

--- eventmaster/settings.py
+++ eventmaster/settings.py
@@ -144,10 +144,7 @@ EMAIL_HOST_PASSWORD = "qazxcv42"
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient"
-        }
+        "LOCATION": "redis://cache://127.0.0.1:6379/1",
     }
 }
 
